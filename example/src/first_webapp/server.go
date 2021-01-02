c6b1c07b1962298aa857ca2e58ae96bb6e9558d9
yuki <yuki.onishi1129@gmail.com>
2021-01-02 18:44:45 +0900

    コメント記載

--- example/src/first_webapp/server.go
+++ example/src/first_webapp/server.go
@@ -1,4 +1,4 @@
-package main
+package main // 実行可能なプログラムは必ず「main」のパッケージの中にある必要あり
 
 import (
 	"fmt"
@@ -9,7 +9,10 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
 
+// バイナリ実行ファイルにコンパイルされる全てのプログラムは「main」関数が1必要
 func main() {
+	// ルートURL('/')が呼び出されたとき、「handler」関数を呼び出す
 	http.HandleFunc("/", handler)
+	// ポート8080でサーバを起動する
 	http.ListenAndServe(":8080", nil)
 }
