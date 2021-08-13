66dbdfa51607dfba85533ba195a7e82b3a154272
yuki <yuki.onishi1129@gmail.com>
2021-08-13 18:13:14 +0900

    add: no condition switch

--- src/go-handson/lesson2/lessonPkg/lessonSwitch.go
+++ src/go-handson/lesson2/lessonPkg/lessonSwitch.go
@@ -26,3 +26,21 @@ func LessonSwitch() {
 		fmt.Println("月に値ではありませんよ？")
 	}
 }
+
+func LessonNoConditionSwitch() {
+	x := hello.Input("type a number")
+	n, err := strconv.Atoi(x)
+
+	if err == nil {
+		fmt.Print(x + "は、")
+	} else {
+		return
+	}
+
+	switch {
+	    case n%2 == 0 :
+			fmt.Println("偶数です。")
+		case n%2 == 1 :
+			fmt.Println("奇数です。")
+	}
+}
--- src/go-handson/lesson2/main.go
+++ src/go-handson/lesson2/main.go
@@ -1,14 +1,14 @@
 package main
 
-import (
-	"go-handson/lesson2/lessonPkg"
-	// 「src」直下から見たパスを記述する
-)
+import "go-handson/lesson2/lessonPkg"
+
+// 「src」直下から見たパスを記述する
 
 func main() {
 	// lessonPkg.LessonInt()
 	// lessonPkg.LessonStrconv()
 	// lessonPkg.LessonConst()
 	// lessonPkg.LessonIf()
-	lessonPkg.LessonSwitch()
+	// lessonPkg.LessonSwitch()
+	lessonPkg.LessonNoConditionSwitch()
 }
