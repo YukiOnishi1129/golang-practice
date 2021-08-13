c7c409855e03f5fa182fb699273c0ba0826430fd
yuki <yuki.onishi1129@gmail.com>
2021-08-13 18:47:41 +0900

    add: slice

--- src/go-handson/lesson2/lessonPkg/lessonArray.go
+++ src/go-handson/lesson2/lessonPkg/lessonArray.go
@@ -9,6 +9,7 @@ import (
 
 func LessonArrayBasic() {
 	x := hello.Input("input data")
+	// 文字列をSplitで配列にして取り出す
 	ar := strings.Split(x, " ")
 	t := 0
 
@@ -24,4 +25,46 @@ func LessonArrayBasic() {
 
 err:
 	fmt.Println("ERROR!")
+}
+
+func LessonArrayRange() {
+	x := hello.Input("input data")
+	ar := strings.Split(x, " ")
+	t := 0
+
+	// range: 配列から順にindexと値を取り出して返す
+	// for文と組み合わせて使う
+	for _, v := range ar {
+		n, er := strconv.Atoi(v)
+		if er != nil {
+			goto err
+		}
+		t += n
+	}
+	fmt.Println("total:", t)
+	return
+err:
+	fmt.Println("ERROR!")
+
+}
+
+func LessonArraySlice() {
+	// 配列をsliceで定義
+	// 変数 := [] 型 {値1, 値2, ...}
+	a := [5]int{10, 20, 30, 40, 50}
+	// 配列からsliceを生成
+	// 変数 := 配列 [開始 : 終了 ]
+	b := a[0:3]
+	fmt.Println(a)
+	fmt.Println(b)
+
+	a[0] = 100
+	// a[0]を変更したのに、b[0]も変更されている
+	// スライスは配列の参照なので、アドレスは同じ
+	fmt.Println(a) // [100 20 30 40 50]
+	fmt.Println(b) // [100 20 30]
+
+	b[1] = 200
+	fmt.Println(a) // [100 200 30 40 50]
+	fmt.Println(b) // [100 200 30]
 }
--- src/go-handson/lesson2/main.go
+++ src/go-handson/lesson2/main.go
@@ -5,11 +5,14 @@ import "go-handson/lesson2/lessonPkg"
 // 「src」直下から見たパスを記述する
 
 func main() {
+	/*
+	 * 2-1　変数、定数
+	 */
 	// lessonPkg.LessonInt()
 	// lessonPkg.LessonStrconv()
 	// lessonPkg.LessonConst()
 	/*
-	 * 2-2
+	 * 2-2　制御構文
 	 */
 	// lessonPkg.LessonIf()
 	// lessonPkg.LessonSwitch()
@@ -19,7 +22,9 @@ func main() {
 	// lessonPkg.LessonForContinue()
 	// lessonPkg.LessonGoto()
 	/*
-	 * 2-3
+	 * 2-3 配列
 	 */
-	lessonPkg.LessonArrayBasic()
+	// lessonPkg.LessonArrayBasic()
+	// lessonPkg.LessonArrayRange()
+	lessonPkg.LessonArraySlice()
 }
