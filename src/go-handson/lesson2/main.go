234f1371894ef532cd46f6df85a50d0df1cd970a
yuki <yuki.onishi1129@gmail.com>
2021-08-13 16:59:30 +0900

    add: const

--- src/go-handson/lesson2/main.go
+++ src/go-handson/lesson2/main.go
@@ -8,11 +8,19 @@ import (
 )
 
 func main() {
+	lessonInt()
+	lessonStrconv()
+	lessonConst()
+}
+
+func lessonInt() {
 	// 数値型
 	a,b,c := 100, 200, 300
 	fmt.Print("total:")
 	fmt.Println(a+b+c)
+}
 
+func lessonStrconv() {
 	// 文字列　→ 数値の型変換
 	// strconv
 	//  x := hello.
@@ -30,4 +38,16 @@ func main() {
 
 	p := float64(n)
 	fmt.Println(int(p * 1.1))
+}
+
+func lessonConst() {
+	// 定数
+	// 型を固定化すると
+	const n1 int = 100
+	// これでエラー
+	// m := n1 * 1.1
+
+	const n2 = 100
+	m := n2 * 1.1
+	fmt.Println(m)
 }
